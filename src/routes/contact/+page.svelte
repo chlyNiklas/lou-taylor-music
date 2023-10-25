774d700be28a25eb5c1077f6d65f655812644927
schreifuchs <schreifuchs@kim.local>
2023-10-25 09:38:47 +0200

    ☎️  contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,30 @@
+<div class="content">
+  <article class="left">
+    <h1>Get in touch with me</h1>
+  </article>
+</div>
+<style lang="scss">
+  @import "$lib/settings.scss";
+
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  img {
+    max-width: 100%;
+  }
+
+  .content {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .left {
+    grid-column: 1 / span 2;
+  }
+  .right {
+    grid-column: 2 / span 3;
+  }
+</style>
